87d2ae85acae59ae3f1257e69926f08bfb83953a
soldamatlab <matoussoldat@email.cz>
2022-03-03 13:35:47 +0100

    add options to run script

--- model/fieldtrip/run_model_fieldtrip.m
+++ model/fieldtrip/run_model_fieldtrip.m
@@ -13,14 +13,30 @@ cfg.path.output = [outputPath '\' run];
 
 %% Segmented MRI
 dataPath = 'C:\Users\matou\Documents\MATLAB\BP_MIDA\data\out';
-cfg.mriSegmented.path = [dataPath '\test\mrtim\ANDROVICOVA_RENATA\01\anatomy_prepro_segment.nii'];
-cfg.mriSegmented.method = 'mrtim';
-cfg.mriSegmented.nLayers = 12;
+
+% MR-TIM segmentation:
+%cfg.mriSegmented.path = [dataPath '\test\mrtim\ANDROVICOVA_RENATA\01\anatomy_prepro_segment.nii'];
+%cfg.mriSegmented.method = 'mrtim';
+%cfg.mriSegmented.nLayers = 12; % 6 or 12
+
+%FieldTrip segmentation:
+cfg.mriSegmented.path = [dataPath '\segmentation_fieldtrip_test\01\mri_segmented.mat'];
+cfg.mriSegmented.method = 'fieldtrip';
+cfg.mriSegmented.nLayers = 5;
 
 %% Miscellaneous
 cfg.visualize = true;
 
+%% For manual run of parts of model_fieldtrip.m
 %Config = cfg; clear cfg; % for manual run of parts of the pipeline
+
+% Import here and skip '%% Import' in model_fieldtrip.m
+% (Active folder should be 'ATiN_MIDA_Matous_project\model\fieldtrip'.)
+%addpath(genpath('./'));
+%addpath('..\..\common');
+
+% Comment initialization of [elecTemplatePath] in model_fieldtrip.m
+%elecTemplatePath = '..\data\elec_template\GSN-HydroCel-257.sfp';
 
 %% Run
 model_fieldtrip(cfg);
