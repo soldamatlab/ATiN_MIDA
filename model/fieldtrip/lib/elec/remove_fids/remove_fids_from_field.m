8f5d121d39a7f4b36c22b0a73c1a6ab9d778b7e8
soldamatlab <matoussoldat@email.cz>
2022-03-30 21:17:45 +0200

    fix remove fids

--- model/fieldtrip/lib/elec/remove_fids/remove_fids_from_field.m
+++ model/fieldtrip/lib/elec/remove_fids/remove_fids_from_field.m
@@ -1,10 +1,16 @@
-function [elecStruct] = remove_fids_from_field(elecStruct, fieldName)
+function [elecStruct] = remove_fids_from_field(elecStruct, fieldName, twoDim)
 if ~isfield(elecStruct, fieldName)
     return
 end
 
 field = elecStruct.(fieldName);
-field = field(4:end,4:end);
+
+if exist('twoDim', 'var') && twoDim
+    field = field(4:end,4:end);
+else
+    field = field(4:end,:);
+end
+
 elecStruct.(fieldName) = field;
 end
 
