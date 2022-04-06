c9b5a7070a77352bf9d973431e15f007855b49a7
soldamatlab <matoussoldat@email.cz>
2022-04-06 10:37:00 +0200

    wg ratio -> gw ratio

--- evaluation/segmentation/metrics/get_gray_white_ratio.m
+++ evaluation/segmentation/metrics/get_gray_white_ratio.m
@@ -1,4 +1,4 @@
-function [wgRatio] = get_wg_ratio(Config, mriSegmented)
+function [gwRatio] = get_gray_white_ratio(Config, mriSegmented)
 %% Import
 wd = fileparts(mfilename('fullpath'));
 addpath(genpath([wd '\..\..\..\common']));
@@ -10,9 +10,9 @@ check_required_field(Config, 'nLayers');
 mriSegmented = load_mri_anytype(mriSegmented);
 
 %% Compute
-white = get_white(Config, mriSegmented);
 gray = get_gray(Config, mriSegmented);
-absWhite = sum(white, 'all');
+white = get_white(Config, mriSegmented);
 absGray = sum(gray, 'all');
-wgRatio = absWhite / absGray;
+absWhite = sum(white, 'all');
+gwRatio = absGray / absWhite;
 end
