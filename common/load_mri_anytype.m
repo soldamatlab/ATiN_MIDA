ca5adbc94223a0fc143ee1aa465d975519d31181
soldamatlab <matoussoldat@email.cz>
2022-04-06 13:54:56 +0200

    add .nhdr and .nrrd support

--- common/load_mri_anytype.m
+++ common/load_mri_anytype.m
@@ -6,6 +6,8 @@ if isstring(mri)
         mri = load_var_from_mat('mriSegmented', mri);
     elseif ext == ".nii" || ext == ".dcm" || ext == ".IMA"
         mri = ft_read_mri(mri);
+    elseif ext == ".nhdr" || ext == ".nrrd"
+        mri = load_nrrd_mri(mri);
     else
         error("Unsupported filetype of segmented MRI.")
     end
