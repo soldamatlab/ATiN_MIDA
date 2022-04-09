6850e4eda8771b941b64218c8f42ca70d7dc8c00
soldamatlab <matoussoldat@email.cz>
2022-04-09 13:53:24 +0200

    add common import

--- scripts/toolbox_paths.m
+++ scripts/toolbox_paths.m
@@ -8,3 +8,9 @@ addpath([matlabroot '\toolbox\spm12'])
 %% MR-TIM
 addpath([matlabroot '\toolbox\spm12\toolbox\mrtim'])
 mrtim
+
+%% Common
+% Relative path
+addpath(genpath('../common'))
+% Absolute path
+%addpath(genpath('C:\Users\matou\Documents\MATLAB\BP_MIDA\ATiN_MIDA_Matous_project\common'))
