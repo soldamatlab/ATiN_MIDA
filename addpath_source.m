52ef7323d0fffcf92d318eb4aab17bacf77c4fe3
soldamatlab <matoussoldat@email.cz>
2022-04-28 01:10:41 +0200

    innit tooloxes

--- addpath_source.m
+++ addpath_source.m
@@ -1,4 +1,17 @@
 function [sourceRoot] = addpath_source()
+%% Define toolbox paths here:
+FIELDTRIP_PATH = [matlabroot '\toolbox\fieldtrip'];
+SPM_PATH = [matlabroot '\toolbox\spm12'];
+MRTIM_PATH = [SPM_PATH '\toolbox\MRTIM'];
+
+%%
+addpath(FIELDTRIP_PATH);
+ft_defaults
+
+addpath(SPM_PATH);
+
+addpath(MRTIM_PATH);
+
 sourceRoot = fileparts(mfilename('fullpath'));
 addpath(genpath(sourceRoot));
 end
