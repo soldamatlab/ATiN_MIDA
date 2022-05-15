0dc67549a509e6f619bc053d2fbdf631b52fe29b
soldamatlab <matoussoldat@email.cz>
2022-05-15 22:45:21 +0200

    calculate mean from aligned maps and plot

--- scripts/surrogate/create_mean_map.m
+++ scripts/surrogate/create_mean_map.m
@@ -1,3 +1,7 @@
+% It is advised to not run the whole script automatically but to run
+% 'Align maps' section first, check the 'finished' cell array if all maps
+% were succesfully aligned and THEN run 'Create mean maps' section.
+
 %% Init
 clear variables
 close all
@@ -5,10 +9,8 @@ addpath_source;
 
 %% Paths & Config - Set manually
 % Local paths:
-%Path.root = 'C:\Users\matou\Documents\MATLAB\BP_MIDA\data'; % NTB
 %Path.root = 'S:\BP_MIDA'; % PC-MATOUS
 Path.root = '\\Pc-matous\bp_mida'; % PC-MATOUS remote
-%Path.root = 'S:\matous'; % PC-SIMON
 
 Path.data.root = [Path.root '\data'];
 Path.data.NUDZ = [Path.data.root '\MR'];
@@ -74,4 +76,106 @@ for s = 1:nSubjects
             finished(s,e) = -1;
         end
     end
+end
+
+%% Create mean maps
+% Change manually: --------------------------------------------------------
+mapNames = {'ed1x' 'ed1y' 'ed1z' 'ed2x' 'ed2y' 'ed2z'};
+evalName = cell(4,1);
+evalName{1} = 'fieldtrip3_anatomy_prepro-fieldtrip3_anatomy_prepro_simulation';
+evalName{2} = 'fieldtrip5_anatomy_prepro-fieldtrip5_anatomy_prepro_simulation';
+evalName{3} = 'fieldtrip3_anatomy_prepro-mrtim12_simulation';
+%evalName{4} = 'fieldtrip5_anatomy_prepro-mrtim12_simulation';
+evalName{4} = 'mrtim12-mrtim12_simulation';
+
+for e = 1:length(evalName)
+    outputPath = [Path.root '\results\surrogate\' dataset '\' evalName{e}];
+    [outputPath, imgPath] = create_output_folder(outputPath, false);
+    % -------------------------------------------------------------------------
+    map = struct;
+
+    %% Gather aligned maps
+    data = struct;
+    nMapNames = length(mapNames);
+    for s = 1:nSubjects
+        evalDir = [Path.(subjects(s).name).surrogate.root '\' evalName{e}];
+        sourcePath = [evalDir '\aligned\source_interp.mat'];
+        source = load_var_from_mat('sourceInterp', sourcePath);
+
+        if s == 1
+            map.anatomy = source.anatomy;
+            map.coordsys = source.coordsys;
+            map.dim = source.dim;
+            map.transform = source.transform;
+            map.unit = source.unit;
+
+            for m = 1:nMapNames
+                data.(mapNames{m}) = NaN([map.dim nSubjects]);
+            end
+        end
+
+        if ~isequal(source.dim, map.dim)
+            error("~isequal(source.dim, map.dim) for subject '%s'", subjects(s).name)
+        end
+
+        for m = 1:nMapNames
+            source.(mapNames{m})(isnan(source.(mapNames{m}))) = 0;
+            data.(mapNames{m})(:,:,:,s) = source.(mapNames{m});
+        end
+    end
+
+    for m = 1:nMapNames
+        if sum(isnan(data.(mapNames{m})))
+            error("'data.%s' contains NaN values.", mapNames{m})
+        end
+    end
+
+    %% Calculate Mean map and STD map
+    for m = 1:nMapNames
+        map.(mapNames{m})      = struct;
+        map.(mapNames{m}).mean = mean(data.(mapNames{m}), 4);
+        map.(mapNames{m}).std  = std(data.(mapNames{m}), 0, 4);
+    end
+    save([outputPath '\mean_map'], 'map');
+
+    %% Prepare plot config
+    mriTarget = load_mri_anytype(Path.mriTarget.(dataset), MRI_TARGET_VAR_NAME);
+    cfg = struct;
+    cfg.crosshair = 'no'; % default
+    cfg.location = 'center';
+    cfg.parameter = 'tmp';
+    cfg.mri = mriTarget;
+    cfg.visible = false;
+    cfg.visualize = false;
+
+    for m = 1:nMapNames    
+        %% Plot Mean map
+        map.tmp = map.(mapNames{m}).mean;
+        cfg.name = [mapNames{m} '_mean'];
+        cfg.save = [imgPath '\' cfg.name];
+        plot_source(cfg, map);
+
+        %% Plot STD map
+        map.tmp = map.(mapNames{m}).std;
+        cfg.name = [mapNames{m} '_std'];
+        cfg.save = [imgPath '\' cfg.name];
+        plot_source(cfg, map);
+    end
+    
+    cfg.method = 'slice';
+    for m = 1:nMapNames    
+        %% Plot Mean map
+        map.tmp = map.(mapNames{m}).mean;
+        cfg.name = ['slice_' mapNames{m} '_mean'];
+        cfg.save = [imgPath '\' cfg.name];
+        plot_source(cfg, map);
+
+        %% Plot STD map
+        map.tmp = map.(mapNames{m}).std;
+        cfg.name = ['slice_' mapNames{m} '_std'];
+        cfg.save = [imgPath '\' cfg.name];
+        plot_source(cfg, map);
+    end
+    
+    map = rmfield(map, 'tmp');
 end
